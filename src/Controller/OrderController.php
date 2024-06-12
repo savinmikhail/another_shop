493c36019b2db16623a0aaf25fcfb8d39f51dcd2
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-12 20:56:31 +0700

    feat: order

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use App\Service\NotificationService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class OrderController extends AbstractController
+
+{
+    public function __construct()
+    {
+    }
+
+    #[Route('/api/order', name: 'create_order', methods: ['POST'])]
+    public function createOrder(Request $request, EntityManagerInterface $em, NotificationService $notificationService): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        // Validate and process the order...
+
+        $order = new Order();
+        // Set order properties...
+
+        $em->persist($order);
+        $em->flush();
+
+        // Send notification...
+        $notificationService->sendEmail($data['userEmail'], 'Order Created', 'Your order has been created.');
+
+        return $this->json(['status' => 'Order created']);
+    }
+}
---
+++ src/Entity/Order.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(inversedBy: 'orders', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
