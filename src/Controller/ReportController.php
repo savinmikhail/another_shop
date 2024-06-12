22a284563857401bae03fab2a8a5e5a27c5ed1bd
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-12 20:56:54 +0700

    feat: report controller

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class ReportController extends AbstractController
+{
+    public function __construct()
+    {
+    }
+}
