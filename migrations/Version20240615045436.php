ba73ceed55465b2dfb3d53c042a87be9bd25d941
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-15 12:11:46 +0700

    fix: migrations

--- migrations/Version20240615045436.php
+++ migrations/Version20240615045436.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240612130036 extends AbstractMigration
+final class Version20240615045436 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,18 +21,27 @@ final class Version20240612130036 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE cart_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE cart_item_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE measurement_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE "order_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE product_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE cart (id INT NOT NULL, owner_id INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_BA388B77E3C61F9 ON cart (owner_id)');
+        $this->addSql('CREATE TABLE cart_item (id INT NOT NULL, product_id INT NOT NULL, cart_id INT NOT NULL, cost INT NOT NULL, quantity INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_F0FE25274584665A ON cart_item (product_id)');
+        $this->addSql('CREATE INDEX IDX_F0FE25271AD5CDBF ON cart_item (cart_id)');
         $this->addSql('CREATE TABLE measurement (id INT NOT NULL, weight INT NOT NULL, height INT NOT NULL, width INT NOT NULL, length INT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE "order" (id INT NOT NULL, owner_id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "order" (id INT NOT NULL, owner_id INT NOT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_F52993987E3C61F9 ON "order" (owner_id)');
         $this->addSql('CREATE TABLE product (id INT NOT NULL, measurement_id INT NOT NULL, cart_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, cost INT NOT NULL, tax INT NOT NULL, version INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D34A04AD924EA134 ON product (measurement_id)');
         $this->addSql('CREATE INDEX IDX_D34A04AD1AD5CDBF ON product (cart_id)');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, phone VARCHAR(15) NOT NULL, role INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
         $this->addSql('ALTER TABLE cart ADD CONSTRAINT FK_BA388B77E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE cart_item ADD CONSTRAINT FK_F0FE25274584665A FOREIGN KEY (product_id) REFERENCES product (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE cart_item ADD CONSTRAINT FK_F0FE25271AD5CDBF FOREIGN KEY (cart_id) REFERENCES cart (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE "order" ADD CONSTRAINT FK_F52993987E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD924EA134 FOREIGN KEY (measurement_id) REFERENCES measurement (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD1AD5CDBF FOREIGN KEY (cart_id) REFERENCES cart (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -43,16 +52,22 @@ final class Version20240612130036 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE cart_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE cart_item_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE measurement_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE "order_id_seq" CASCADE');
         $this->addSql('DROP SEQUENCE product_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
         $this->addSql('ALTER TABLE cart DROP CONSTRAINT FK_BA388B77E3C61F9');
+        $this->addSql('ALTER TABLE cart_item DROP CONSTRAINT FK_F0FE25274584665A');
+        $this->addSql('ALTER TABLE cart_item DROP CONSTRAINT FK_F0FE25271AD5CDBF');
         $this->addSql('ALTER TABLE "order" DROP CONSTRAINT FK_F52993987E3C61F9');
         $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD924EA134');
         $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD1AD5CDBF');
         $this->addSql('DROP TABLE cart');
+        $this->addSql('DROP TABLE cart_item');
         $this->addSql('DROP TABLE measurement');
         $this->addSql('DROP TABLE "order"');
         $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE "user"');
     }
 }
