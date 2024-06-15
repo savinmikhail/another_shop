ba04dbe17e3360d1470fa4c5a4727d5120c87b53
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-15 20:17:00 +0700

    feat(notification): logging

--- src/Service/NotificationService.php
+++ src/Service/NotificationService.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
+
 final readonly class NotificationService
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function sendEmail(string $message): void
     {
+        $this->logger->info('Sending email', ['message' => $message]);
         //send to responsible microservice
     }
 
-    public function sendSMS(string $message)
+    public function sendSMS(string $message): void
     {
+        $this->logger->info('Sending SMS', ['message' => $message]);
         //send to responsible microservice
     }
 }
