23fdc64101b0ca8046510b651a4b60a5e66a58b3
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-16 09:38:45 +0700

    refactor: address controller logic to service

--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -4,30 +4,30 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\Address;
-use App\Service\NotificationService;
-use Doctrine\ORM\EntityManagerInterface;
+use App\DTO\Address\CreateAddressDTO;
+use App\Service\AddressService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Annotation\Route;
-
-use function json_decode;
 
 final class AddressController extends AbstractController
 {
+    public function __construct(
+        private readonly AddressService $addressService,
+    ) {
+    }
+
     #[Route('/api/address', name: 'create_address', methods: ['POST'])]
     public function createOrder(
-        Request $request,
-        EntityManagerInterface $em,
+        #[MapRequestPayload] CreateAddressDTO $createAddressDTO
     ): Response {
-        $data = json_decode($request->getContent(), true);
-        $address = new Address();
-        $address
-            ->setFullAddress($data['fullAddress'])
-            ->setKladrId($data['kladrId']);
-        $em->persist($address);
-        $em->flush();
-        return $this->json('address was saved successfully', Response::HTTP_OK);
+        try {
+            $this->addressService->create($createAddressDTO);
+            return $this->json('Address was saved successfully', Response::HTTP_OK);
+        } catch (Exception $e) {
+            return $this->json($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
 }
