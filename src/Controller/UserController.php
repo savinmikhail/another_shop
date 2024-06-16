79cf8510633b75e5390330b1feccad9bd8f8ae00
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-16 08:33:34 +0700

    style: user controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
 use function json_decode;
 
 final class UserController extends AbstractController
