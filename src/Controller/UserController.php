b3414348d876946933253ecdcaa1ae3640c88009
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-16 15:34:27 +0700

    refactor: user controller change role

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,23 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\DTO\Order\CreateOrderDTO;
+use App\DTO\User\UserEditRoleDTO;
 use App\DTO\User\UserRegisterDTO;
-use App\Enum\UserRole;
 use App\Service\UserService;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-
-use function json_decode;
 
 final class UserController extends AbstractController
 {
@@ -33,7 +25,6 @@ final class UserController extends AbstractController
     public function register(
         #[MapRequestPayload] UserRegisterDTO $userRegisterDTO
     ): JsonResponse {
-
         try {
             $this->userService->register($userRegisterDTO);
             return new JsonResponse(['message' => 'User registered successfully'], Response::HTTP_CREATED);
@@ -44,14 +35,13 @@ final class UserController extends AbstractController
 
     #[Route('/api/admin/user', name: 'admin_user_edit', methods: ['PATCH'])]
     public function edit(
-        Request $request,
-        EntityManagerInterface $em
+        #[MapRequestPayload] UserEditRoleDTO $editRoleDTO
     ): JsonResponse {
-        $data = json_decode($request->getContent(), true);
-        $user = $em->getRepository(User::class)->find($data['id']);
-        $user->setRole(UserRole::tryFrom($data['role']));
-        $em->persist($user);
-        $em->flush();
-        return new JsonResponse(['message' => 'User data saved successfully'], Response::HTTP_OK);
+        try {
+            $this->userService->editRole($editRoleDTO);
+            return new JsonResponse(['message' => 'User data saved successfully'], Response::HTTP_OK);
+        } catch (Exception $e) {
+            return $this->json($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
 }
