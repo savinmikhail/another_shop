b61d516e22de9b15f60f616f2f37e65cb948eae4
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-16 16:00:49 +0700

    feat: create product dto

---
+++ src/DTO/Product/CreateProductDTO.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO\Product;
+
+use App\DTO\Measurement\MeasurementDTO;
+use Symfony\Component\Validator\Constraints as Assert;
+
+final readonly class CreateProductDTO
+{
+    public function __construct(
+        #[Assert\Optional]
+        #[Assert\Type('integer')]
+        #[Assert\GreaterThan(0)]
+        public ?int $id,
+        #[Assert\NotBlank]
+        #[Assert\Type('string')]
+        public string $name,
+        #[Assert\NotBlank]
+        #[Assert\Valid]
+        public MeasurementDTO $measurements,
+        #[Assert\NotBlank]
+        #[Assert\Type('string')]
+        public string $description,
+        #[Assert\NotBlank]
+        #[Assert\Type('integer')]
+        #[Assert\GreaterThan(0)]
+        public int $cost,
+        #[Assert\NotBlank]
+        #[Assert\Type('integer')]
+        #[Assert\GreaterThan(0)]
+        public int $tax,
+        #[Assert\NotBlank]
+        #[Assert\Type('integer')]
+        #[Assert\GreaterThan(0)]
+        public int $version
+    ) {
+    }
+}
