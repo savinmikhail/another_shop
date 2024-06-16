9b903a04659bb4a152ef300b51b6e4b36144f1b2
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-16 16:02:14 +0700

    refactor: product controller create

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,53 +4,35 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\Measurement;
+use App\DTO\Product\CreateProductDTO;
 use App\Entity\Product;
+use App\Service\ProductService;
+use Exception;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use function json_decode;
-
 final class ProductController extends AbstractController
 {
-    public function __construct()
-    {
+    public function __construct(
+        private readonly ProductService $productService,
+    ) {
     }
 
     #[Route('/api/product', name: 'add_product', methods: ['POST'])]
     public function create(
-        Request $request,
-        EntityManagerInterface $em
+        #[MapRequestPayload] CreateProductDTO $createProductDTO,
     ): JsonResponse {
-        $data = json_decode($request->getContent(), true);
-        if ($data['id']) {
-            $product = $em->find(Product::class, $data['id']);
-        } else {
-            $product = new Product();
+        try {
+            $this->productService->create($createProductDTO);
+            return new JsonResponse('product was added successfully', Response::HTTP_CREATED);
+        } catch (Exception $e) {
+            return $this->json($e->getMessage(), Response::HTTP_BAD_REQUEST);
         }
-        $measurement = $product->getMeasurement() ?? new Measurement();
-        $measurement
-            ->setWeight($data['measurements']['weight'])
-            ->setHeight($data['measurements']['height'])
-            ->setLength($data['measurements']['length'])
-            ->setWidth($data['measurements']['width'])
-            ;
-        $product
-            ->setName($data['name'])
-            ->setDescription($data['description'])
-            ->setCost($data['cost'])
-            ->setTax($data['tax'])
-            ->setVersion($data['version'])
-            ->setMeasurement($measurement);
-        $em->persist($product);
-        $em->flush();
-
-        return new JsonResponse(['product' => $product], Response::HTTP_CREATED);
     }
 
     #[Route('/api/products', name: 'get_products', methods: ['GET'])]
