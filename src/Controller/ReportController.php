394c692ff3314f054807062b5d64b3ac22c0369d
Mikhail <salazar290720035017@gmail.com>
2024-06-23 23:06:56 +0700

    refactor: report controller using kafka

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -4,69 +4,30 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\CartItem;
-use App\Entity\OrderItem;
+use App\DTO\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Uid\Uuid;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class ReportController extends AbstractController
 {
+    public function __construct(private readonly MessageBusInterface $messageBus)
+    {
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
     #[Route('/api/report/generate', name: 'generate_report', methods: ['POST'])]
-    public function generateReport(EntityManagerInterface $em, HttpClientInterface $httpClient): JsonResponse
+    public function generateReport(): JsonResponse
     {
         $reportId = Uuid::v4()->toRfc4122();
-
-        $this->generateReportAsync($reportId, $em, $httpClient);
+        $this->messageBus->dispatch(new Message($reportId));
 
         return new JsonResponse(['reportId' => $reportId], Response::HTTP_ACCEPTED);
-    }
-
-    private function generateReportAsync(
-        string $reportId,
-        EntityManagerInterface $em,
-        HttpClientInterface $httpClient
-    ): void {
-        $reportFilePath = $this->generateReportFile($reportId, $em);
-
-        // Send an event to Kafka about the report generation status
-        $result = [
-            'reportId' => $reportId,
-            'result' => 'success',
-            'detail' => []
-        ];
-
-//        $httpClient->request('POST', 'http://kafka-service/notify', [
-//            'json' => $result
-//        ]);
-    }
-
-    private function generateReportFile(string $reportId, EntityManagerInterface $em): string
-    {
-        $soldItems = $em->getRepository(OrderItem::class)->findAll();
-
-        $filePath = __DIR__ . '/../../var/reports/' . $reportId . '.jsonl';
-        $fileHandle = fopen($filePath, 'w');
-
-        foreach ($soldItems as $item) {
-            $reportLine = json_encode([
-                'product_name' => $item->getProduct()->getName(),
-                'price' => $item->getCost(),
-                'amount' => $item->getQuantity(),
-                'user' => [
-                    'id' => $item->getOrder()->getOwner()->getId()
-                ]
-            ]);
-
-            fwrite($fileHandle, $reportLine . PHP_EOL);
-        }
-
-        fclose($fileHandle);
-
-        return $filePath;
     }
 }
