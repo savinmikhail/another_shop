2d790c0b55cce2a9277c577d37130b54b842c429
Mikhail <salazar290720035017@gmail.com>
2024-06-30 15:43:12 +0700

    fix: address validation

--- src/DTO/Address/CreateAddressDTO.php
+++ src/DTO/Address/CreateAddressDTO.php
@@ -9,10 +9,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 final readonly class CreateAddressDTO
 {
     public function __construct(
-        #[Assert\Optional]
         #[Assert\Type('string')]
         public ?string $fullAddress,
-        #[Assert\Optional]
         #[Assert\Type('integer')]
         #[Assert\GreaterThan(0)]
         public ?int $kladrId,
