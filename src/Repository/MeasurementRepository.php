836f7746a558a2316dde5150475aa00e6cb08dd4
Mikhail <salazar290720035017@gmail.com>
2024-07-09 13:16:55 +0300

    refactor: remove commented code in presotories

--- src/Repository/MeasurementRepository.php
+++ src/Repository/MeasurementRepository.php
@@ -15,29 +15,4 @@ class MeasurementRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Measurement::class);
     }
-
-    //    /**
-    //     * @return Measurement[] Returns an array of Measurement objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Measurement
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
