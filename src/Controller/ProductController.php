58bf32ec699e71a71a10a6b584ded430f9c7b45b
Mikhail <salazar290720035017@gmail.com>
2024-07-19 20:27:51 +0300

    refactor: controller imports

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\DTO\Product\CreateProductDTO;
-use App\DTO\Product\FindProductRequest;
+use App\DTO\Product\Request\CreateProductDTO;
+use App\DTO\Product\Request\FindProductRequest;
 use App\Service\ProductService;
 use Exception;
-use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class ProductController extends AbstractController
 {
