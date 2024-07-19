e652c47d9e8941ba16196071ae5423d75a19fd7e
Mikhail <salazar290720035017@gmail.com>
2024-07-19 20:26:42 +0300

    fix: measurement to product reference

--- src/Entity/Measurement.php
+++ src/Entity/Measurement.php
@@ -15,48 +15,23 @@ class Measurement
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: "SEQUENCE")]
     #[ORM\Column]
-    #[Groups(['product:read'])]
     private ?int $id = null;
 
-    #[ORM\OneToOne(mappedBy: 'measurement', cascade: ['persist', 'remove'])]
-    private ?Product $product = null;
-
     #[ORM\Column]
-    #[Groups(['product:read'])]
     private ?int $weight = null;
 
     #[ORM\Column]
-    #[Groups(['product:read'])]
     private ?int $height = null;
 
     #[ORM\Column]
-    #[Groups(['product:read'])]
     private ?int $width = null;
 
     #[ORM\Column]
-    #[Groups(['product:read'])]
     private ?int $length = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getProduct(): ?Product
-    {
-        return $this->product;
-    }
-
-    public function setProduct(Product $product): static
-    {
-        // set the owning side of the relation if necessary
-        if ($product->getMeasurements() !== $this) {
-            $product->setMeasurements($this);
-        }
-
-        $this->product = $product;
-
-        return $this;
     }
 
     public function getWeight(): ?int
