226c3d3ac3d27995d41bd2b7551678780b16f14c
Mikhail <salazar290720035017@gmail.com>
2024-07-19 18:40:43 +0300

    feat: controller search product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\DTO\Product\CreateProductDTO;
+use App\DTO\Product\FindProductRequest;
 use App\Service\ProductService;
 use Exception;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
@@ -12,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ProductController extends AbstractController
 {
@@ -37,6 +39,21 @@ final class ProductController extends AbstractController
     {
         try {
             $response = $this->productService->index();
+            return new JsonResponse($response, Response::HTTP_OK, [], true);
+        } catch (Exception $e) {
+            return $this->json($e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    #[Route('/api/products/search', name: 'search_products', methods: ['GET'])]
+    public function search(Request $request): JsonResponse {
+        try {
+            $findProductRequest = new FindProductRequest(
+                search: $request->query->get('search')
+            );
+
+            $response = $this->productService->search($findProductRequest);
+
             return new JsonResponse($response, Response::HTTP_OK, [], true);
         } catch (Exception $e) {
             return $this->json($e->getMessage(), Response::HTTP_BAD_REQUEST);
